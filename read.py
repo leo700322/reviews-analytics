8e59e9c0b644717e2ad6c9d527c4f4ee684bb685
jay yeh <leo700322@gmail.com>
2022-08-28 14:20:13 +0800

    add read.py for count and print once in 10000 times

---
+++ read.py
@@ -0,0 +1,16 @@
+data = []
+count = 0
+with open('reviews.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 10000 == 0: # % 主要用來求餘數
+            print(len(data))
+            
+        
+print(len(data))
+
+print(data[0])
+print('----------')
+print(data[1])
+
